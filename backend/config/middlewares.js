726e8c9ed5c4b3013b5acb60b7e61037054397b0
Romsha Wadhwa <romsha2303@gmail.com>
2026-08-19 02:29:23 +0530

    fix: configure CORS for production deployment

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'https://localhost:3000',
+        process.env.FRONTEND_URL || '*',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
